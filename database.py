dfb7c1c38edb830e0c19dcf98491c45d6e373468
aqulline <aqeglipambuya@gmail.com>
2024-05-21 11:14:26 +0300

    database already some reports ongoing

--- database.py
+++ database.py
@@ -1,3 +1,6 @@
+import json
+
+
 class FireBase:
     def Register_user(self, phone, username, password):
         import firebase_admin
@@ -68,7 +71,7 @@ class FireBase:
             try:
                 cred = credentials.Certificate("credential/farmzon-abdcb-c4c57249e43b.json")
                 initialize_app(cred, {'databaseURL': 'https://farmzon-abdcb.firebaseio.com/'})
-                store = db.reference("Ceremony").child(ceremony_name).child('Info_Company')
+                store = db.reference("Ceremony").child(ceremony_name).child('Info_Ceremony')
                 store.set({
                     "Attended": 0,
                 })
@@ -81,3 +84,94 @@ class FireBase:
                 })
             except:
                 return "No Internet!"
+
+    def search_id(self, gen_id):
+        import firebase_admin
+        from firebase_admin import credentials, initialize_app, db
+
+        # Clear existing Firebase apps to avoid conflicts
+        firebase_admin._apps.clear()
+
+        try:
+            # Initialize Firebase app with credentials and database URL
+            cred = credentials.Certificate("credential/farmzon-abdcb-c4c57249e43b.json")
+            initialize_app(cred, {'databaseURL': 'https://farmzon-abdcb.firebaseio.com/'})
+
+            # Get a reference to the Firebase database node containing user information
+            users_ref = db.reference("Ceremony")
+
+            # Query the database to find the user with the specified gen_id
+            user_info = None
+            for ceremony_name in users_ref.get().keys():
+                ceremony_ref = users_ref.child(ceremony_name)
+                for phone_number in ceremony_ref.get().keys():
+                    user_ref = ceremony_ref.child(phone_number).child('User_Info')
+                    user_data = user_ref.get()
+                    if user_data and user_data.get("user_password") == gen_id:
+                        # Found the user with the specified gen_id
+                        user_info = user_data
+                        break
+                if user_info:
+                    break
+
+            return user_info
+
+        except Exception as e:
+            return f"Error: {e}"
+
+    def scan_guest(self, gen_id):
+        import firebase_admin
+        from firebase_admin import credentials, initialize_app, db
+
+        # Clear existing Firebase apps to avoid conflicts
+        firebase_admin._apps.clear()
+
+        try:
+            # Initialize Firebase app with credentials and database URL
+            cred = credentials.Certificate("credential/farmzon-abdcb-c4c57249e43b.json")
+            initialize_app(cred, {'databaseURL': 'https://farmzon-abdcb.firebaseio.com/'})
+
+            # Get a reference to the Firebase database node containing user information
+            users_ref = db.reference("Ceremony")
+
+            # Query the database to find the user with the specified gen_id
+            user_info = None
+            user_ref_to_update = None
+            ceremony_name_to_update = None
+            for ceremony_name in users_ref.get().keys():
+                ceremony_ref = users_ref.child(ceremony_name)
+                for phone_number in ceremony_ref.get().keys():
+                    user_ref = ceremony_ref.child(phone_number).child('User_Info')
+                    user_data = user_ref.get()
+                    if user_data and user_data.get("user_password") == gen_id:
+                        # Found the user with the specified gen_id
+                        user_info = user_data
+                        user_ref_to_update = user_ref
+                        ceremony_name_to_update = ceremony_name
+                        break
+                if user_info:
+                    break
+
+            if user_info:
+                if user_info.get("scanned") == 0:
+                    # Update the scanned field to 1
+                    user_ref_to_update.update({"scanned": 1})
+
+                    # Increment the Attended field
+                    attended_ref = users_ref.child(ceremony_name_to_update).child('Info_Ceremony').child('Attended')
+                    attended_count = attended_ref.get()
+                    attended_ref.set(attended_count + 1)
+
+                    return "User scanned status updated to 1 and Attended count incremented"
+                else:
+                    return "User has already been scanned"
+            else:
+                return "User not found"
+
+        except Exception as e:
+            return f"Error: {e}"
+
+
+# x = FireBase.scan_guest(FireBase(), "20240510112246919903")
+# print(x)
+
--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+import re
+from datetime import datetime
+import threading
+
+from PIL import Image
 from kivy.base import EventLoop
-from kivy.properties import NumericProperty, StringProperty, DictProperty, ListProperty, BooleanProperty
+from kivy.properties import NumericProperty, StringProperty, DictProperty, ListProperty, BooleanProperty, ObjectProperty
 from kivymd.app import MDApp
 from kivy.core.window import Window
-from kivy.clock import Clock
+from kivy.clock import Clock, mainthread
 from kivy import utils
 import qrcode
-from kivymd.toast import toast
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.card import MDCard
-from kivymd.uix.list import IRightBodyTouch, TwoLineAvatarIconListItem
-from kivymd.uix.selectioncontrol import MDCheckbox
+from kivymd.uix.dialog import MDDialog
+from pyzbar.pyzbar import decode
+from kivymd.toast import toast
+from camera4kivy import Preview
+from kivymd.uix.textfield import MDTextField
 
 from database import FireBase as FB
 
@@ -20,17 +28,120 @@ if utils.platform != 'android':
     Window.size = (412, 732)
 
 
+class Spin(MDBoxLayout):
+    pass
+
+
+class Front(MDCard):
+    pass
+
+
+class Scan_Analyze(Preview):
+    extracted_data = ObjectProperty(None)
+
+    def analyze_pixels_callback(self, pixels, image_size, image_pos, scale, mirror):
+
+        pimage = Image.frombytes(mode='RGBA', size=image_size, data=pixels)
+        list_of_all_barcodes = decode(pimage)
+
+        if list_of_all_barcodes:
+            if self.extracted_data:
+                self.extracted_data(list_of_all_barcodes[0])
+            else:
+                print("NOt found")
+
+
+class NumberOnlyField(MDTextField):
+    pat = re.compile('[^0-9]')
+
+    input_type = "number"
+
+    def insert_text(self, substring, from_undo=False):
+
+        pat = self.pat
+
+        if "." in self.text:
+            s = re.sub(pat, "", substring)
+
+        else:
+            s = ".".join([re.sub(pat, "", s) for s in substring.split(".", 1)])
+
+        return super(NumberOnlyField, self).insert_text(s, from_undo=from_undo)
+
+
 class MainApp(MDApp):
     # app
     size_x, size_y = Window.size
+    dialog_spin = None
 
     # screen
     screens = ['home']
     screens_size = NumericProperty(len(screens) - 1)
     current = StringProperty(screens[len(screens) - 1])
 
+    # user
+    guest_name = StringProperty("loading...")
+    guest_number = StringProperty("loading...")
+    ceremony_name = StringProperty("loading...")
+    guest_id = StringProperty("loading...")
+    guest_data = DictProperty({})
 
-    def qr_code(self):
+    # fetched
+    guest_scanned = StringProperty("")
+    guest_fetch_name = StringProperty("")
+    guest_fetch_phone = StringProperty("")
+    guest_datas = {}
+
+    def on_start(self):
+        if utils.platform == 'android':
+            self.request_android_permissions()
+        self.keyboard_hooker()
+
+    def keyboard_hooker(self, *args):
+        EventLoop.window.bind(on_keyboard=self.hook_keyboard)
+
+    def hook_keyboard(self, window, key, *largs):
+        print(self.screens_size)
+        if key == 27 and self.screens_size > 0:
+            print(f"your were in {self.current}")
+            last_screens = self.current
+            self.screens.remove(last_screens)
+            print(self.screens)
+            self.screens_size = len(self.screens) - 1
+            self.current = self.screens[len(self.screens) - 1]
+            self.screen_capture(self.current)
+            return True
+        elif key == 27 and self.screens_size == 0:
+            toast('Press Home button!')
+            return True
+
+    def spin_dialog(self):
+        if not self.dialog_spin:
+            self.dialog_spin = MDDialog(
+                type="custom",
+                auto_dismiss=False,
+                size_hint=(.43, None),
+                content_cls=Spin(),
+            )
+        self.dialog_spin.open()
+
+    def generate_guest_id(self):
+        # timestamp = int(time() * 1000)  # Convert current time to milliseconds
+        refer = str(datetime.now())
+        refer = refer.replace('-', '').replace(':', '').replace(' ', '').replace('.', '')
+        return str(refer)
+
+    def get_guest(self, phone, name, ceremony_name):
+        print(phone, name, ceremony_name)
+        self.ceremony_name = ceremony_name
+        self.guest_name = name
+        self.guest_number = phone
+        self.guest_id = self.generate_guest_id()
+        self.qr_code(self.guest_id, self.guest_number)
+
+        FB.add_attender(FB(), self.guest_number, self.guest_name, self.guest_id, self.ceremony_name)
+
+    def qr_code(self, id_gen, phone):
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -38,11 +149,58 @@ class MainApp(MDApp):
             border=4,
         )
 
-        qr.add_data("WeddingQrcode")
+        qr.add_data(id_gen)
         qr.make(fit=True)
         img = qr.make_image(fill_color="black", back_color="white")
-        img.save("example.png")
-        
+        img.save(f"Qrcodes/{phone}.png")
+
+    def get_details(self):
+        self.root.ids.details_scan.connect_camera(enable_analyze_pixels=True, default_zoom=0.0)
+        print("connected")
+
+    def stop_camera_detail(self):
+        self.root.ids.details_scan.disconnect_camera()
+
+    @mainthread
+    def get_QRcode(self, result):
+        barcode = str(result.data)
+        code_type = str(result.type)
+        print(barcode)
+        if barcode:
+            if code_type == "QRCODE":
+                barcode = barcode.replace("b", "").replace("'", "")
+
+                self.root.ids.pda.text = barcode
+
+                self.spin_dialog()
+                # guest_data = FB.search_id(FB(), barcode)
+
+                thr = threading.Thread(target=self.get_data)
+                thr.start()
+
+    def get_data(self):
+        barcode = self.root.ids.pda.text
+        self.guest_data = FB.search_id(FB(), barcode)
+        if self.guest_data:
+            self.guest_fetch_name = self.guest_data['user_name']
+            self.guest_fetch_phone = self.guest_data['user_phone']
+            self.guest_scanned = str(self.guest_data['scanned'])
+            Clock.schedule_once(lambda dt: self.dialog_spin.dismiss(), 0)
+
+    def verify_user(self, gen_id):
+        FB.scan_guest(FB(), gen_id)
+
+    def request_android_permissions(self):
+        from android.permissions import request_permissions, Permission
+
+        def callback(permissions, results):
+            if all([res for res in results]):
+                print("callback. All permissions granted.")
+            else:
+                print("callback. Some permissions refused.")
+
+        request_permissions([Permission.CAMERA], callback)
+
     def screen_capture(self, screen):
         sm = self.root
         sm.current = screen
